2cc4f976706b7af37ab909c85dce39ea5278268f
Jwcrist <jacecrist@gmail.com>
2014-11-13 13:47:25 -0600

    Update
    Update 11/13

--- Data folder/server.R
+++ Data folder/server.R
@@ -1,70 +1,51 @@
-
 library(ggplot2)
 library(scales)
 library(ggmap)
 library(dplyr)
 
-# crimeDat <- readRDS("usaCrimeDat.rds") %>%
-#   filter(state != 'district of columbia')
-crimeDat <- read.csv("life-expectancy.csv")
-
-
+StreamlineDat <- read.csv("life-expectancy.csv")
+#gsub()
 mdat <- map_data("state")
 
 shinyServer(function(input, output) {
   
-  # setting the reactive environment 
-  dataInput <- reactive({
-    crimeDat[,c(input$myVar1,input$myVar2)]
-  })
+  dataInput<-reactive({
   
-  
-    output$myMap <- renderPlot({ 
-      myDat <- dataInput()
-      colnames(myDat) <- c("v1","v2")
-      ggplot(myDat, aes(x=v1, y=v2)) + geom_point()
-
-    })
-
-  output$myPlot <- renderPlot({ 
-    dd <- dataInput()
-    qplot(dd[,1],dd[,2])
+    mYear<-input$myYears
+    mMonths<-input$myMonths
     
+    StreamlineDat1<-StreamlineDat %>%
+      filter(
+          YR=mYear,
+          MO=mMonths)
+    
+#   StreamlineDat %>%
+#       select(YR=input$myYears, MO=input$myMonths)
+  
+#    StreamlineDat[,c(input$myVar1,input$myVar2,input$myVar3)]
+ 
+    StreamlineDat1[,c(input$myVar1,input$myVar2,input$myVar3)]
+      
   }) 
-  
-  # PLotting the bar plots
-#   output$myPlot <- renderPlot({
-#     ggplot(dataInput(), aes(reorder(state,rate),rate))+ 
-#       geom_point(color="steelblue", size=4)+
-#       coord_flip() + theme_bw() 
-#     
-#   })
-  
-  # Plotting the usa map
-#   output$myMap <- renderPlot({ 
-#     combdat <- merge(mdat, dataInput(), by.x=c('region'), 
-#                      by.y=c('state'), all.x=TRUE)
-#     odat <- combdat[order(combdat$order),]
-#     ggplot(odat, aes(x=long, y=lat,group=group)) +
-#       geom_polygon(aes(fill=rate), colour = alpha("white", 0.2)) + 
-#       theme_bw() + scale_fill_continuous(low="blue", high="pink") +
-#       theme(
-#         legend.position = "none",
-#         text = element_blank(), 
-#         line = element_blank()) 
-#   })
-#   
-#   output$myCrime <- renderPlot({ 
-#     sDat <- dataInput() %>% 
-#       arrange(-rate)
-#     topState <- sDat$state[1]
-#     subDat <- subset(crimeDat,state==topState)
-#     ggplot(subDat, aes(x=Year, y=rate,color=Crime)) + geom_line()+
-#       theme_bw() + ggtitle(topState)+ theme(legend.position="bottom") + 
-#       guides(colour = guide_legend(nrow = 2))+ 
-#       ylab("Rate/10,000 people")
-#   })
-#   
+
+
+output$myPlot<-renderPlot({
+  myDat<-dataInput()
+  colnames(myDat)<-c('v1','v2','v3')
+  Titlemaps<-paste("Plot of ",input$myVar1, " vs ",input$myVar2," faceted by ", input$myVar3)
+  ggplot(myDat,aes(x=v1,y=v2, alpha = 0.15,color=v3))+
+    geom_point()+theme(axis.text.x = element_text(angle=90,hjust=1))+facet_wrap(~v3)+
+    labs(title = Titlemaps,x=input$myVar1,y=input$myVar2)
+})
+  output$myMap<-renderPlot({
+    myDat<-dataInput()
+    colnames(myDat)<-c('v1','v2','v3')
+    Titlemaps<-paste("Plot of ",input$myVar1, " vs ",input$myVar2," faceted by ", input$myVar3)
+    ggplot(myDat,aes(x=v1,y=v2,alpha=0.25))+
+      geom_smooth()+theme(axis.text.x = element_text(angle=90,hjust=1))+facet_wrap(~v3)+
+      labs(title = Titlemaps,x=input$myVar1,y=input$myVar2)
+  })    
+    
 })
 
 
--- Data folder/ui.R
+++ Data folder/ui.R
@@ -1,66 +1,72 @@
-
-
-#crimeDat <- readRDS("usaCrimeDat.rds")
-crimeDat <- read.csv("life-expectancy.csv")
-
-
-myVariables <- as.character(colnames(crimeDat))
+StreamlineDat <- read.csv("life-expectancy.csv")
+myVariables <- as.character(unique(colnames(StreamlineDat)))
 
 # Define UI for application that draws a histogram
-shinyUI(fluidPage(
+shinyUI(navbarPage("Streamline Data",id="nav",
   ###################### first row starts here
+tabPanel("Explore Data:",
   fluidRow(
     column(12,
            # Application title takes all 12 column spaces
-           titlePanel("My Shiny application to explore US crime data")
+           titlePanel("Application to explore 'Streamline' buying patterns")
     )),
   
   ###################### 2nd row starts here. 2 columns 
   fluidRow(
-    column(5,
+    column(6,
            wellPanel(
              selectInput("myVar1", 
-                         label = "Choose Variable to display",
-                         choices = myVariables, 
-                         selected = myVariables[1]),
-             
-             selectInput("myVar2", 
-                         label = "Choose Variable to display",
+                         label = "Choose X variable to display",
                          choices = myVariables, 
                          selected = myVariables[3]),
+             
+             selectInput("myVar2", 
+                         label = "Choose Y variable to display",
+                         choices = myVariables, 
+                         selected = myVariables[4]),
+             
+             selectInput("myVar3",
+                         label = "Choose faceting variable",
+                         choices = myVariables,
+                         selected = myVariables[2]),
             
              sliderInput("myYears",
                          label = "Year of Transportation",
-                         min = 1996,
-                         max = 2014,
-                         value = 1996,
+                         min = 1960,
+                         max = 2012,
+                         value = 1965,
                          step=1),
              
-             sliderInput("myYears",
-                         "Crime Year",
-                         min = 1969,
-                         max = 2005,
-                         value = 1980),
-             "This is a project created to demonstrate how shiny application 
-              can be used for exploratory data analysis. For more information 
-              about this project please visit github repository 
-              https://github.com/mamajumder/usa-crime"
+             sliderInput("myMonths",
+                         label = "Month of Transportation",
+                         min = 01,
+                         max = 12,
+                         value = 9),
+             
+             checkboxInput("checkbox", 
+                           label = "Buys Only?", 
+                           value = TRUE),
+                     
+             "This is a project created to explore Streamline's buying patterns
+             of customers who visit their webpage."
            )
     ),
     column(6, 
-           plotOutput("myMap",height = "300px", width="550px")
+           plotOutput("myMap",height = "450px", width="500px")
     )      
   ),
   
   ###################### 3rd row starts here. 2 columns
   fluidRow(
     column(5, 
-           plotOutput("myPlot",height = "900px",width="350px")
+           plotOutput("myPlot",height = "750px",width="750px")
     ),
     column(6, 
-           tableOutput("t3")
+           plotOutput("myCrime",height = "400px", width="550px")
     )  
   )
   
   
+),
+tabPanel("Model:")
 ))
